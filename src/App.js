506b943db6058802d096b4046cbf5e83b92eeae2
sumancrest0001 <suman.crest0001@gmail.com>
2021-01-04 14:47:05 +0545

    Complete footer section with finalized styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import Jumbotron from './component/jumbotron/jumbotron';
-import jumboData from './fixtures/jumbo';
+import JumbotronContainer from './containers/Jumbotron.container';
+import FooterContainer from './containers/Footer.container';
 
 function App() {
   return (
-    <Jumbotron.Container>
-      {
-        jumboData.map(item => (
-          <Jumbotron key={item.id} direction={item.direction}>
-            <Jumbotron.Pane>
-              <Jumbotron.Title>{item.title}</Jumbotron.Title>
-              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-            </Jumbotron.Pane>
-            <Jumbotron.Pane>
-              <Jumbotron.Image src={item.image} alt={item.alt} />
-            </Jumbotron.Pane>
-          </Jumbotron>
-        ))
-      }
-    </Jumbotron.Container>
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
   );
 }
 
---
+++ src/containers/Footer.container.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Footer } from '../component';
+
+const FooterContainer = () => (
+  <Footer>
+    <Footer.Title>
+      Questions? Contact us.
+    </Footer.Title>
+    <Footer.Break />
+    <Footer.Row>
+      <Footer.Link href="#">FAQs</Footer.Link>
+      <Footer.Link href="#">Media Center</Footer.Link>
+      <Footer.Link href="#">Ways to Watch</Footer.Link>
+      <Footer.Link href="#">Cookie Preferences</Footer.Link>
+      <Footer.Link href="#">Speed Test</Footer.Link>
+      <Footer.Link href="#">Help Center</Footer.Link>
+      <Footer.Link href="#">INvestor Relations</Footer.Link>
+      <Footer.Link href="#">Term of Use</Footer.Link>
+      <Footer.Link href="#">Corporate Information</Footer.Link>
+      <Footer.Link href="#">Legal Notices</Footer.Link>
+      <Footer.Link href="#">Account</Footer.Link>
+      <Footer.Link href="#">Jobs</Footer.Link>
+      <Footer.Link href="#">Privacy</Footer.Link>
+      <Footer.Link href="#">Contact Us</Footer.Link>
+      <Footer.Link href="#">Netflix Originals</Footer.Link>
+    </Footer.Row>
+    <Footer.Break />
+    <Footer.Text>Netflix Nepal</Footer.Text>
+  </Footer>
+)
+
+export default FooterContainer;
+
